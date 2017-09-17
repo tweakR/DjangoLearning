61ffaf5ffa6568d5de703717da6b29b3376d060e
Viacheslav Ivankov <Viacheslav@asta.mobi>
2017-09-17 18:49:50 +0300

    front page now generated from html

--- tdd/functional_test.py
+++ tdd/functional_test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome("/home/viacheslav/chromedriver/chromedriver")
+        self.driver = webdriver.Chrome("C:\\chromedriver/chromedriver")
         self.addCleanup(self.driver.quit)
 
     def tearDown(self):
@@ -18,14 +18,15 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         input_box = self.driver.find_element_by_id('id_new_item')
-        self.assertEqual(input_box.get_attribute('place_holder'), 'Enter a to-do item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
         input_box.send_keys('Buy peacock feathers')
 
         input_box.send_keys(Keys.ENTER)
 
         table = self.driver.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item didn't appear in "
+                                                                                    "table")
 
 
 if __name__ == '__main__':
--- tdd/tests.py
+++ tdd/tests.py
@@ -1,9 +1,9 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 
 from tdd.views import home_page
-from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
