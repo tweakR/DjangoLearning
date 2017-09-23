2554d9c7cd294efb65a1ab216957ecdaa682d8c3
Viacheslav Ivankov <Viacheslav@asta.mobi>
2017-09-23 20:28:49 +0300

    added a model's test

--- tdd/tests.py
+++ tdd/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from tdd.models import Item
 from tdd.views import home_page
 
 
@@ -15,7 +16,7 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode('utf8'), expected_html)
+        self.assertEqual(response.content.decode(), expected_html)
 
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -29,5 +30,24 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
-        # expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
-        # self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item second')
--- tdd/views.py
+++ tdd/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home_page(request):
